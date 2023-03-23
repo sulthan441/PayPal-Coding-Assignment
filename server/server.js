2b46d677d4c16a566ea889b93430126fc2ccc775
sulthan441 <mohammedsulthan441@gmail.com>
2023-03-24 02:14:51 +0530

    connected to database

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config()
+// import Connection from './database/db';
+const Connection = require('./database/db')
 const app = express();
 app.use(cors());
 
@@ -9,5 +11,5 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }))
 
 const PORT = process.env.PORT || 8000;
-
+Connection()
 app.listen(PORT, () => console.log(`Your server is running successfully on PORT ${PORT}`));
