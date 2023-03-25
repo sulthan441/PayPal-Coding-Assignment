9461a369aec8992685991065598768093c9b090d
sulthan441 <mohammedsulthan441@gmail.com>
2023-03-25 14:35:59 +0530

    tasks part completed with ui

--- client/src/components/TaskItem.js
+++ client/src/components/TaskItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import "../App.css"
 const TaskItem = ({ task, handleStatusChange, handleAssigneeChange }) => {
 
   const handleStatusSelect = (event) => {
--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import TaskItem from './TaskItem';
 import axios from 'axios';
+import "../App.css"
 const TaskList = () => {
 
 const [tasks,setTasks] = useState([])
@@ -30,7 +31,7 @@ const [tasks,setTasks] = useState([])
 
   const getTasks = async()=>{
     try {
-     let data = await axios.get('http://localhost:5000/api/tasks')
+     let data = await axios.get('http://localhost:8000/api/tasks')
      setTasks(data.data)
      console.log(data)
     
